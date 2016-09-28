0577fb2236a3acec7762e65d39afa8974521efc2
Yangzhedi <uiryzd@163.com>
2016-09-29 00:27:36 +0800

    index

---
+++ js/Components/Chart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+let Chart = React.createClass({
+
+    render() {
+        return(
+            <div>
+                <img src="../../imgs/dongtu.gif"/>
+                <span style={{display:"block"}}>This is chart!</span>
+            </div>
+        );
+    }
+});
+
+export default Chart;  
---
+++ js/app.js
@@ -0,0 +1,31 @@
+import React from 'react';  
+import Router from 'react-router';  
+import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
+
+import LoginHandler from './components/Login.js';
+import ChartHandler from './components/Chart.js';
+
+let App = React.createClass({
+    render() {
+        return (
+            <div className="nav">
+                <Link to="app">Home</Link>
+                <Link to="login">Login</Link>
+                <Link to="chart">Chart</Link>
+                {/* this is the importTant part*/}
+                <RouteHandler/>
+            </div>
+        );
+    }
+});
+
+let routes = (  
+    <Route name="app" path="/" handler={App}>
+        <Route name="login" path="/login" handler={LoginHandler}/>
+        <Route name="chart" path="/chart" handler={ChartHandler}/>
+    </Route>
+);
+
+Router.run(routes, function (Handler) {  
+    React.render(<Handler/>, document.body);
+});
